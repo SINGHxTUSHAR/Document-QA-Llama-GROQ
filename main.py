e31e0d77fbc2bb6f1c6d2123385c0bf4cda03088
SINGHxTUSHAR <tusharsinghrawat.delhi@gmail.com>
2025-03-05 12:38:03 +0530

    main-webApp

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
 groq_api_key=os.getenv("GROQ_API_KEY")
 
